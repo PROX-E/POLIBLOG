3ee08d59b988d8803792c8e6d8a6c78b9401d88b
PROX-E <lichlord18@gmail.com>
2024-06-10 21:45:40 -0500

    update server

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const sqlite3 = require('sqlite3').verbose();
 const session = require('express-session');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
