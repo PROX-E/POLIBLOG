7abbec03091ba5a474fbb3df453ae9d1f801083d
PROX-E <lichlord18@gmail.com>
2024-06-10 21:46:08 -0500

    update server
    Connect to the SQLite database file

--- server.js
+++ server.js
@@ -9,3 +9,20 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+
+// Connect to the SQLite database file
+const db = new sqlite3.Database('./blog.db', (err) => {
+    if (err) {
+        console.error('Could not connect to database', err);
+    } else {
+        console.log('Connected to the SQLite database.');
+    }
+});
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+}));
