722e6a4d6846097e9a5b17da35447f9709d4752f
PROX-E <lichlord18@gmail.com>
2024-06-13 18:57:18 -0500

    update search.js

--- search.js
+++ search.js
@@ -3,5 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const title = urlParams.get('title');
     const date = urlParams.get('date');
 
-    
+    fetch(`/search-posts?title=${encodeURIComponent(title)}&date=${encodeURIComponent(date)}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                displaySearchResults(data.posts);
+            } else {
+                alert('Error searching posts');
+            }
+        });
 });
