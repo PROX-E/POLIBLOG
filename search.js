aec10ab4eddd4db1ad12f875ed117e7ae4edc577
PROX-E <lichlord18@gmail.com>
2024-06-13 18:56:44 -0500

    update search.js
    add parameters for search function

---
+++ search.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const title = urlParams.get('title');
+    const date = urlParams.get('date');
+
+    
+});
